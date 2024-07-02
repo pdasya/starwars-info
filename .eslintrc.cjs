eaa7dbcad8f1f94e250d161987e2f9cce1c2788b
Daria Pashkova <dar.pashcovaa@gmail.com>
2024-07-02 11:37:02 +0900

    feat: update eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,11 +9,27 @@ module.exports = {
     ignorePatterns: ['dist', '.eslintrc.cjs'],
     parser: '@typescript-eslint/parser',
     plugins: ['react-refresh'],
+    parserOptions: {
+      ecmaVersion: 2020,
+      sourceType: 'module',
+      project: ['./tsconfig.app.json', './tsconfig.node.json'],
+    },
+    env: {
+      es6: true,
+      browser: true,
+      node: true,
+    },
     rules: {
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
       ],
+      'no-param-reassign': 'off',
+      'import/extensions': 'off',
+      'no-console': 'warn',
+      'import/prefer-default-export': 'off',
+      '@typescript-eslint/no-explicit-any': 'error',
+      'no-nested-ternary': 'off',
     },
   }
   
