e05344f33965ace489dff53ff4d1ef7fcf765b92
Daria Pashkova <dar.pashcovaa@gmail.com>
2024-07-02 11:45:01 +0900

    feat: add and apply prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,38 +1,35 @@
 module.exports = {
-    root: true,
-    env: { browser: true, es2020: true },
-    extends: [
-      'eslint:recommended',
-      'plugin:@typescript-eslint/recommended',
-      'plugin:react-hooks/recommended',
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+  ],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh", "react-compiler"],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module",
+    project: ["./tsconfig.app.json", "./tsconfig.node.json"],
+  },
+  env: {
+    es6: true,
+    browser: true,
+    node: true,
+  },
+  rules: {
+    "react-refresh/only-export-components": [
+      "warn",
+      { allowConstantExport: true },
     ],
-    ignorePatterns: ['dist', '.eslintrc.cjs'],
-    parser: '@typescript-eslint/parser',
-    plugins: ['react-refresh',
-       "react-compiler"
-    ],
-    parserOptions: {
-      ecmaVersion: 2020,
-      sourceType: 'module',
-      project: ['./tsconfig.app.json', './tsconfig.node.json'],
-    },
-    env: {
-      es6: true,
-      browser: true,
-      node: true,
-    },
-    rules: {
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-      'no-param-reassign': 'off',
-      'import/extensions': 'off',
-      'no-console': 'warn',
-      'import/prefer-default-export': 'off',
-      '@typescript-eslint/no-explicit-any': 'error',
-      'no-nested-ternary': 'off',
-      "react-compiler/react-compiler": "error"
-    },
-  }
-  
+    "no-param-reassign": "off",
+    "import/extensions": "off",
+    "no-console": "warn",
+    "import/prefer-default-export": "off",
+    "@typescript-eslint/no-explicit-any": "error",
+    "no-nested-ternary": "off",
+    "react-compiler/react-compiler": "error",
+  },
+};
