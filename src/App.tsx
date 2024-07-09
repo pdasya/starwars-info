98bb0c549ec38006ecd9c3ae68290dc8afcc240f
Daria Pashkova <dar.pashcovaa@gmail.com>
2024-07-09 15:29:03 +0900

    feat: implement basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
 import "./App.css";
 import Main from "./views/main";
 import ErrorBoundary from "./components/error-boundary-component/error-boundary-component";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 
-const App = () => {
+const App: React.FC = () => {
   return (
-    <ErrorBoundary>
-      <Main />
-    </ErrorBoundary>
+    <Router>
+      <ErrorBoundary>
+        <Routes>
+          <Route path="/main" element={<Main />} />
+          <Route path="/" element={<Navigate replace to="/main" />} />
+          <Route path="*" element={<h1>404 Not Found</h1>} />
+        </Routes>
+      </ErrorBoundary>
+    </Router>
   );
 };
 
