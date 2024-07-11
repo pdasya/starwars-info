7854b5ea1c8801647b54118de2ed21fade3d8b0d
Daria Pashkova <dar.pashcovaa@gmail.com>
2024-07-11 11:23:30 +0900

    fix: fix pagination errors

--- src/views/main/main.tsx
+++ src/views/main/main.tsx
@@ -14,14 +14,20 @@ const Main: FC = () => {
   const [searchResults, setSearchResults] = useState<Character[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [searchParams, setSearchParams] = useSearchParams();
-  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [currentPage, setCurrentPage] = useState<number>(
+    Number(localStorage.getItem("currentPage")),
+  );
   const [totalPages, setTotalPages] = useState<number>(1);
   const [selectedCharacter, setSelectedCharacter] = useState<Character | null>(
     null,
   );
-  const [isItemOpened, setIsItemOpened] = useState<boolean>(false);
+  const [isDetailLoading, setIsDetailLoading] = useState<boolean>(false);
 
+  // const navigate = useNavigate();
   const searchQuery = searchParams.get("search") || "";
+  const pageQuery =
+    searchParams.get("page") || localStorage.getItem("currentPage");
+  // const detailsQuery = searchParams.get("details") || "";
 
   const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
     setSearchTerm(event.target.value.toString());
@@ -48,9 +54,23 @@ const Main: FC = () => {
     }
   };
 
+  const fetchCharacterDetails = async (character: Character) => {
+    setIsDetailLoading(true);
+    try {
+      const response = await fetch(character.url);
+      const data = await response.json();
+      setSelectedCharacter(data);
+      setIsDetailLoading(false);
+    } catch (error) {
+      console.error("Error fetching character details:", error);
+      setIsDetailLoading(false);
+    }
+  };
+
   useEffect(() => {
-    const page = parseInt(searchParams.get("page") || "1", 10);
-    setCurrentPage(page);
+    const page = Number(pageQuery);
+    // setCurrentPage(page);
+    // console.log(currentPage);
 
     if (searchQuery) {
       setSearchTerm(searchQuery);
@@ -58,49 +78,109 @@ const Main: FC = () => {
     } else {
       handleSearch(searchTerm, page);
     }
-  }, [searchQuery, searchParams]);
+
+    // if (detailsQuery) {
+    //   const character = searchResults.find(c => c.url.split('/').slice(-2, -1)[0] === detailsQuery);
+    //   if (character) {
+    //     fetchCharacterDetails(character);
+    //   }
+    // }
+  }, [searchQuery, pageQuery]);
 
   const handlePageChange = (page: number) => {
+    setSearchParams(
+      {
+        search: searchTerm,
+        page: page.toString(),
+        details: selectedCharacter
+          ? selectedCharacter.url.split("/").slice(-2, -1)[0]
+          : "",
+      },
+      { replace: false },
+    );
     setCurrentPage(page);
     handleSearch(searchTerm, page);
   };
 
+  useEffect(() => {
+    if (currentPage !== Number(pageQuery)) {
+      setSearchParams(
+        {
+          search: searchTerm,
+          page: currentPage.toString(),
+          details: selectedCharacter
+            ? selectedCharacter.url.split("/").slice(-2, -1)[0]
+            : "",
+        },
+        { replace: false },
+      );
+    }
+
+    localStorage.setItem("currentPage", currentPage.toString());
+  }, [currentPage]);
+
   const handleItemClick = (character: Character) => {
-    setSelectedCharacter(character);
-    setIsItemOpened(true);
+    fetchCharacterDetails(character);
+    setSearchParams(
+      {
+        search: searchTerm,
+        page: currentPage.toString(),
+        details: character.url.split("/").slice(-2, -1)[0],
+      },
+      { replace: false },
+    );
   };
 
   const handleItemClose = () => {
-    setIsItemOpened(false);
+    setSearchParams(
+      { search: searchTerm, page: currentPage.toString() },
+      { replace: false },
+    );
+    setSelectedCharacter(null);
   };
 
   return (
-    <>
-      <Search
-        searchTerm={searchTerm}
-        onInputChange={handleInputChange}
-        onSearch={() => handleSearch(searchTerm, 1)}
-      />
-      {isLoading ? (
-        <div className={styles.overlay}>
-          <span className={styles.loader}>
-            <span className={styles.loaderInner}></span>
-          </span>
-        </div>
-      ) : (
-        <>
-          <Result results={searchResults} onItemClick={handleItemClick} />
-          <Pagination
-            currentPage={currentPage}
-            totalPages={totalPages}
-            onPageChange={handlePageChange}
-          />
-          {selectedCharacter && isItemOpened && (
+    <div className={styles.mainContainer}>
+      <div
+        className={`${styles.searchResults} ${selectedCharacter ? styles.withDetails : ""}`}
+        onClick={handleItemClose}
+      >
+        <Search
+          searchTerm={searchTerm}
+          onInputChange={handleInputChange}
+          onSearch={() => handleSearch(searchTerm, 1)}
+        />
+        {isLoading ? (
+          <div className={styles.overlay}>
+            <span className={styles.loader}>
+              <span className={styles.loaderInner}></span>
+            </span>
+          </div>
+        ) : (
+          <>
+            <Result results={searchResults} onItemClick={handleItemClick} />
+            <Pagination
+              currentPage={currentPage}
+              totalPages={totalPages}
+              onPageChange={handlePageChange}
+            />
+          </>
+        )}
+      </div>
+      {selectedCharacter && (
+        <div className={styles.detailsSection}>
+          {isDetailLoading ? (
+            <div className={styles.overlay}>
+              <span className={styles.loader}>
+                <span className={styles.loaderInner}></span>
+              </span>
+            </div>
+          ) : (
             <Details details={selectedCharacter} onClose={handleItemClose} />
           )}
-        </>
+        </div>
       )}
-    </>
+    </div>
   );
 };
 
